6d9d3f4b8680fa5744f4cf15f4fed6ef9589246c
Zachary <syyun@snu.ac.kr>
2019-02-03 13:04:14 +0900

    Auto stash before merge of "master" and "origin/sub_master"

--- download/parse_url.py
+++ download/parse_url.py
@@ -1,13 +1,9 @@
+import os
 import pickle
 
-if __name__ == "__main__":
-    path = "./wto_pdf_urls.pkl"
-    with open(path, 'rb') as f:
-        pkl_f = pickle.load(f)
-        print(pkl_f.keys())
-        print(pkl_f)
-        print(pkl_f[1])
-        print(len(pkl_f[1]))
 
-        print(pkl_f[2])
-        print(len(pkl_f[2]))
+if __name__ == '__main__':
+    pkl_file_path = "./wto_pdf_urls.pkl"
+    
+    with open(pkl_file_path, 'rb') as f:
+        pkl_f = pickle.load(f)
