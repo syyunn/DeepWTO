6474f96a59472e53b832fff403b52fd378af462b
Zachary <syyun@snu.ac.kr>
2019-02-03 18:24:52 +0900

    Add panel/appellate body filter

--- database/parse_url.py
+++ database/parse_url.py
@@ -10,7 +10,8 @@ def split_and_filter(intermediate_str, splitter):
     :return: chunk of string(s) as a list
     """
     intermediate_split = intermediate_str.split(splitter)
-    intermediate_filter = [elem for elem in intermediate_split if 'https' in elem]
+    intermediate_filter = [elem for elem in intermediate_split
+                           if 'https' in elem]
     return intermediate_filter[0]
 
 
@@ -25,28 +26,39 @@ def standard_parse(string):
     return second_output
 
 
-class ParseUrl:
-    def __init__(self, string):
-        self.string = string
+def parse_one(string):
+    result_str = standard_parse(string)
+    return result_str
 
-    def parse(self):
-        result_str = standard_parse(self.string)
-        return result_str
+
+class ParseUrl:
+    """
+    Parse the raw string crawled and extract valid 'https:xxx.pdf'
+    """
+    def __init__(self, pkl_path):
+        
+        with open(pkl_path, 'rb') as f:
+            self.target = pickle.load(f)
+    
+    def parse_all(self):
+        new_dict = dict()
+        for key in self.target.keys():
+            new_dict[key] = []
+            for raw_str in result[key]:
+                parsed = parse_one(raw_str)
+                new_dict[key].append(parsed)
+        return new_dict
 
 
 if __name__ == '__main__':
     pkl_file_path = "./wto_pdf_urls.pkl"
+    parsed_pkl_outpath = "./parsed.pkl"
 
     with open(pkl_file_path, 'rb') as f:
         result = pickle.load(f)
 
-    print(result.keys())
-    counter = 0
-    for key in result.keys():
-        counter += len(result[key])
-        print(len(result[key]))
-        if len(result[key]) == 0:
-            print(key)
-    print(counter)
-
-
+    parser = ParseUrl(pkl_file_path)
+    parsed_dict = parser.parse_all()
+    
+    with open(parsed_pkl_outpath, 'wb') as f:
+        pickle.dump(parsed_dict, f)
