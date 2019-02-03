5e7d79b7795ce131fc3e9a44cf2927f9a97ffb47
Zachary <syyun@snu.ac.kr>
2019-02-03 13:14:25 +0900

    Structify the class

--- download/download.py
+++ download/download.py
@@ -1,10 +1,8 @@
 import pickle
-import collections
 from multiprocessing import Pool
 
 
 from selenium import webdriver
-from tqdm import tqdm
 
 
 def get_pdf_urls(chrome_driver, dispute_idx):
@@ -14,17 +12,17 @@ def get_pdf_urls(chrome_driver, dispute_idx):
     :param dispute_idx: ds number to retrieve
     :return: string of integer
     """
-    url_prefix = "https://docs.wto.org/dol2fe/Pages/FE_Search/FE_S_S006.aspx" \
-                 "?Query=(@Symbol=%20wt/ds{}/*)&Language=ENGLISH&Context=" \
-                 "FomerScriptedSearch&languageUIChanged=true#".\
-        format(dispute_idx)
+    url_prefix = "https://docs.wto.org/dol2fe/Pages/FE_Search/" \
+                 "FE_S_S006.aspx?Query=(@Symbol=%20wt/ds{}/*)&" \
+                 "Language=ENGLISH&Context=FomerScriptedSearch&" \
+                 "languageUIChanged=true#".format(dispute_idx)
         
     chrome_driver.get(url_prefix)
 
     try:
         page = chrome_driver.\
-            find_element_by_css_selector('#ctl00_MainPlaceHolder_dlPaging > tbody '
-                                         '> tr > td:nth-child(1)')
+            find_element_by_css_selector('#ctl00_MainPlaceHolder_dlPaging '
+                                         '> tbody > tr > td:nth-child(1)')
     except:
         page = None
 
@@ -45,8 +43,8 @@ def get_pdf_urls(chrome_driver, dispute_idx):
         page_idx += 1
         try:
             page = chrome_driver.find_element_by_css_selector(
-                '#ctl00_MainPlaceHolder_dlPaging > tbody > tr > td:nth-child({'
-                '})'.format(page_idx))
+                '#ctl00_MainPlaceHolder_dlPaging > tbody '
+                '> tr > td:nth-child({})'.format(page_idx))
         except:
             break
     print(len(onclick_list))
@@ -54,45 +52,48 @@ def get_pdf_urls(chrome_driver, dispute_idx):
     result[dispute_idx] = onclick_list
     return result
 
-    
-if __name__ == "__main__":
-    chrome_driver_path = "/Users/jjong/Project/DeepWTO/download/chromedriver"
-    # final_ds_numb = 577
-    final_ds_numb = 3
-    total_ds_idxs = [i for i in range(1, final_ds_numb)]
-    n_threads = 128
 
-    merge_dict_outpath = "/Users/jjong/Project/" \
-                         "DeepWTO/download/wto_pdf_urls.pkl"
+class CrawlWTO:
+    def __init__(self,
+                 chrome_driver_path,
+                 final_ds_numb,
+                 outpath,
+                 pool):
+        self.chrome_driver_path = chrome_driver_path
+        self.final_ds_numb = final_ds_numb
+        self.total_ds_idxs = [i for i in range(1, final_ds_numb)]
+        self.outpath = outpath
+        self.pool = pool
 
-
-    def unit_crawl(ds_index):
-        driver_one_time_use = webdriver.Chrome(chrome_driver_path)
+    def unit_crawl(self, ds_index):
+        driver_one_time_use = webdriver.Chrome(self.chrome_driver_path)
         unit_result = get_pdf_urls(driver_one_time_use, ds_index)
         driver_one_time_use.close()
         return unit_result
 
+    def crawl(self):
+        with Pool(2) as p:
+            result_dicts = p.map(self.unit_crawl, self.total_ds_idxs)
+    
+        print(result_dicts)
+        print(type(result_dicts))
+    
+        merge_dict = {}
+        for d in result_dicts:
+            for k, v in d.items():  # d.items() in Python 3+
+                merge_dict[k] = v
+    
+        with open(self.outpath, 'wb') as f:
+            pickle.dump(merge_dict, f)
 
-    # result_dicts = []
-    # for ds_idx in tqdm(total_ds_idxs):
-    #     result_dict = unit_crawl(ds_idx)
-    #     result_dicts.append(result_dict)
+    
+if __name__ == "__main__":
+    downloader = CrawlWTO(chrome_driver_path=
+                          "/Users/zachary/projects/DeepWTO/download"
+                          "/chromedriver",
+                          final_ds_numb=577,
+                          outpath= "/Users/zachary/projects/DeepWTO/download/"
+                                   "wto_pdf_urls.pkl",
+                          pool=30)
 
-    with Pool(2) as p:
-        result_dicts = p.map(unit_crawl, total_ds_idxs)
-
-    print(result_dicts)
-    print(type(result_dicts))
-
-    merge_dict = {}
-    for d in result_dicts:
-        for k, v in d.items():  # d.items() in Python 3+
-            merge_dict[k] = v
-
-    # merge_dict = collections.defaultdict()
-    # for d in result_dicts:
-    #     for k, v in d.items():  # d.items() in Python 3+
-    #         merge_dict[k].append(v)
-
-    with open(merge_dict_outpath, 'wb') as f:
-        pickle.dump(merge_dict, f)
+    downloader.crawl()
