0ab3c784c55d2b082dc6bed39b889bd97da03ed6
Zachary <syyun@snu.ac.kr>
2019-03-01 19:36:02 +0900

    Add gov filter

--- dataset/gov_measure/filter.py
+++ dataset/gov_measure/filter.py
@@ -7,12 +7,25 @@ For given DS_number,
 """
 import os
 import urllib.request
+
+from utils.pdf import read_pdf
 from dataset.download.fetch_pickle import get_urls, filter_eng
 
 
 def download(url):
-    urllib.request.urlretrieve(url, os.path.join("downloads",
-                                                 url.split("/")[-1]))
+    download_path = os.path.join("downloads", url.split("/")[-1])
+    urllib.request.urlretrieve(url, download_path)
+    return download_path
+
+
+def filter_gov(txt):
+    tgt_phrases = ["Request for Consultations", "Request for the "
+                                               "Establishment of a Panel"]
+    for phrase in tgt_phrases:
+        if phrase in txt:
+            return True
+        else:
+            return False
 
 
 def main():
@@ -21,8 +34,14 @@ def main():
     eng_urls = filter_eng(urls)
     print(eng_urls)
     
+    gov_measure = ''
     for eng_url in eng_urls:
-        download(eng_url)
+        fpath = download(eng_url)
+        txt = read_pdf(fpath)
+        boolean_mask = filter_gov(txt)
+        if boolean_mask:
+            gov_measure += txt
+    print(txt)
 
 
 if __name__ == "__main__":
