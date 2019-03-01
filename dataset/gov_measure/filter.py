1b15eb73e409e34de35ad03bb6216e8f662ee5fb
Zachary <syyun@snu.ac.kr>
2019-03-01 20:57:32 +0900

    Add gov_measure filter

--- dataset/gov_measure/filter.py
+++ dataset/gov_measure/filter.py
@@ -7,6 +7,7 @@ For given DS_number,
 """
 import os
 import urllib.request
+from multiprocessing import Pool
 
 from utils.pdf import read_pdf
 from dataset.download.fetch_pickle import get_urls, filter_eng
@@ -19,29 +20,50 @@ def download(url):
 
 
 def filter_gov(txt):
-    tgt_phrases = ["Request for Consultations", "Request for the "
-                                               "Establishment of a Panel"]
+    tgt_phrases = ["Request for Consultations",
+                   "Request for the Establishment of a Panel"]
+    bools = []
     for phrase in tgt_phrases:
         if phrase in txt:
-            return True
-        else:
-            return False
+            bools.append(True)
+    if True in bools:
+        return True
+    else:
+        return False
+
+
+def unit_filter(url):
+    fpath = download(url)
+    txt = read_pdf(fpath)
+    boolean_mask = filter_gov(txt)
+    if boolean_mask:
+        return txt
+    else:
+        return ''
+
+
+def multiprocess_filter(pool_number, urls):
+    with Pool(pool_number) as p:
+        texts = p.map(unit_filter, urls)
+    count = 0
+    gov_measure = ''
+    for text in texts:
+        gov_measure += text
+        if text != '':
+            count += 1
+
+    print(gov_measure)
+    print(count)
+    
+    return gov_measure
 
 
 def main():
     ds_num = 161
     urls = get_urls(ds_num)
     eng_urls = filter_eng(urls)
-    print(eng_urls)
-    
-    gov_measure = ''
-    for eng_url in eng_urls:
-        fpath = download(eng_url)
-        txt = read_pdf(fpath)
-        boolean_mask = filter_gov(txt)
-        if boolean_mask:
-            gov_measure += txt
-    print(txt)
+    pool_num = 20
+    multiprocess_filter(pool_num, eng_urls)
 
 
 if __name__ == "__main__":
