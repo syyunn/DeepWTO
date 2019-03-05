4b9708e9eff50f4a047ba6868b6cb164a0a7c589
Zachary <syyun@snu.ac.kr>
2019-03-05 22:06:38 +0900

    Changed big role

--- dataset/factual/dump.py
+++ dataset/factual/dump.py
@@ -6,22 +6,29 @@ from dataset.factual.parser import PanelParser
 
 
 def main():
-    ds_numb = 162
-    path = "/Users/zachary/Downloads/162R-00.pdf"
-    factual_dict = {}
-    parser = PanelParser(path)
-    pdf, start, end = parser.factual_locator()
+    ds_numb = 161
+    pdf_name = "161R.pdf"
+    pdf_path = "/Users/zachary/Downloads/{}".format(pdf_name)
     
-    factual = ''
-    for page_idx in range(start, end + 1):
-        print(page_idx)
-        part = pdf[page_idx]
-        factual += part
-    print(factual)
-    factual_dict[ds_numb] = factual
+    factual_dict_path = "factual.pkl"
+    with open(factual_dict_path, 'rb') as f:
+        factual_dict = pickle.load(f)
     
-    with open("factual.pkl", 'wb') as f:
-        pickle.dump(factual_dict, f)
+    print(factual_dict[ds_numb])
+    
+    # parser = PanelParser(pdf_path)
+    # pdf, start, end = parser.factual_locator()
+    #
+    # factual = ''
+    # for page_idx in range(start, end + 1):
+    #     print(page_idx)
+    #     part = pdf[page_idx]
+    #     factual += part
+    # print(factual)
+    # factual_dict[ds_numb] = factual
+    #
+    # with open("factual.pkl", 'wb') as f:
+    #     pickle.dump(factual_dict, f)
 
 
 if __name__ == "__main__":
