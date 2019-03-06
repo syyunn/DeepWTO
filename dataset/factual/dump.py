039c0b82286fdbf69481d51c70e3b2ea7c737e1e
Zachary <syyun@snu.ac.kr>
2019-03-06 16:31:27 +0900

    Add 177 linked with178

--- dataset/factual/dump.py
+++ dataset/factual/dump.py
@@ -1,13 +1,14 @@
 """ Extract & Dump the Factual Aspect of Each DS Panel Report into Pickle"""
 
 import pickle
+import pdftotext
 
 from dataset.factual.parser import PanelParser
 
 
 def main():
-    ds_numb = 175
-    pdf_name = "175R.pdf"
+    ds_numb = 177
+    pdf_name = "178R-00.pdf"
     pdf_path = "/Users/zachary/Downloads/{}".format(pdf_name)
     
     factual_dict_path = "factual.pkl"
@@ -20,23 +21,70 @@ def main():
     except:
         print("not yet stored")
     
-    parser = PanelParser(pdf_path)
-    pdf, start, end = parser.factual_locator()
+    # parser = PanelParser(pdf_path)
+    # pdf, start, end = parser.factual_locator()
+    #
+    # print("start page idx: ", start)
+    # print("end page idx: ", end)
+    #
+    # factual = ''
+    # for page_idx in range(start, end + 1):
+    #     print(page_idx)
+    #     part = pdf[page_idx]
+    #     factual += part
+    # # print(factual)
+    # factual_dict[ds_numb] = factual
+    #
+    # with open("factual.pkl", 'wb') as f:
+    #     pickle.dump(factual_dict, f)
 
-    print("start page idx: ", start)
-    print("end page idx: ", end)
 
-    factual = ''
-    for page_idx in range(start, end + 1):
+def add_annex():
+    ds_numb = 177
+    pdf_name = "178R-01.pdf"
+    pdf_path = "/Users/zachary/Downloads/{}".format(pdf_name)
+    
+    factual_dict_path = "factual.pkl"
+    with open(factual_dict_path, 'rb') as f:
+        factual_dict = pickle.load(f)
+    
+    with open(pdf_path, "rb") as file:
+        pdf = pdftotext.PDF(file)
+    
+    start = 6
+    end = 14
+    slice = 23600
+    add = ''
+    
+    for page_idx in range(start, end+1):
         print(page_idx)
-        part = pdf[page_idx]
-        factual += part
-    # print(factual)
-    factual_dict[ds_numb] = factual
+        page = pdf[page_idx]
+        add += page
 
+    print(add)
+    print(len(add))
+    print(add[:slice])
+    tail_cut_add = add[:slice]
+
+    factual_dict_path = "factual.pkl"
+    with open(factual_dict_path, 'rb') as f:
+        factual_dict = pickle.load(f)
+
+    print("stored factuals: ", factual_dict.keys())
+    try:
+        print("stored factual content: \n", len(factual_dict[ds_numb]))
+    except:
+        print("not yet stored")
+    
+    final_facts = factual_dict[ds_numb] + tail_cut_add
+    print(final_facts)
+    print(len(final_facts))
+    
+    factual_dict[ds_numb] = final_facts
     with open("factual.pkl", 'wb') as f:
         pickle.dump(factual_dict, f)
-
+    
 
 if __name__ == "__main__":
     main()
+    # add_annex()
---
+++ dataset/provision/acronym.md
@@ -0,0 +1,11 @@
+### About Acronyms of the Names of Agreements
+      GM: Government Measure At Issue
+      GATT: General Agreement on Tariffs and Trade 1994
+      AOA: Agreement on Agriculture
+      ADA: Agreement on Anti-Dumping
+      SA: Agreement on Safeguards
+      ASCM: Agreement on Subsidies and Countervailing Measures
+      ATC: Agreement on Textiles and Clothing
+      SPS: Agreement on the Application of Sanitary and Phytosanitary Measures
+      TRIPS: The Agreement on Trade-Related Aspects of Intellectual Property Rights
+      WTO: Agreement Establishing the WTO
