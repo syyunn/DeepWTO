7bf41f1ac73642740af401ad8efb0ec2a391eab6
Zachary <syyun@snu.ac.kr>
2019-03-15 16:55:28 +0900

    Add ubuntu env.yaml

--- utils/misc/json.py
+++ utils/misc/json.py
@@ -5,6 +5,7 @@ def dump_write_dict2json(dictionary, write_path):
     with open(write_path, 'w') as f:
         json.dump(dictionary, f)
 
+
 def write_json_line_by_line(list_of_dicts, write_path):
     with open(write_path, "a") as outfile:
         for data in list_of_dicts[:-1]:
