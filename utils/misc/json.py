467e3c5c15b2b36ab81b3bf635e63c7d5ed47b5c
Zachary <syyun@snu.ac.kr>
2019-03-15 14:54:01 +0900

    Add list of dicts writer

--- utils/misc/json.py
+++ utils/misc/json.py
@@ -4,3 +4,11 @@ import json
 def dump_write_dict2json(dictionary, write_path):
     with open(write_path, 'w') as f:
         json.dump(dictionary, f)
+
+def write_json_line_by_line(list_of_dicts, write_path)
+    with open(write_path, "a") as outfile:
+        for data in list_of_dicts[:-1]:
+            json.dump(data, outfile)
+            outfile.write('\n')
+        json.dump(list_of_dicts[-1], outfile)
+
